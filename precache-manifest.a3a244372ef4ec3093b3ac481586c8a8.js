f98af5ec409744d7a34d59864657641accd7cecd
yuriy82791 <yuriy82791@gmail.com>
2020-04-29 16:53:57 +0300

    Updates

---
+++ precache-manifest.a3a244372ef4ec3093b3ac481586c8a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654381c29975c0da2325b4a6f9e5bfbb",
+    "url": "/post-redux/index.html"
+  },
+  {
+    "revision": "5a19866b4766d9bea0b1",
+    "url": "/post-redux/static/css/main.7b969ac5.chunk.css"
+  },
+  {
+    "revision": "c7dd883a5bdb929144bf",
+    "url": "/post-redux/static/js/2.a040c7b3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/post-redux/static/js/2.a040c7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a19866b4766d9bea0b1",
+    "url": "/post-redux/static/js/main.d6bcb1aa.chunk.js"
+  },
+  {
+    "revision": "fbd23d2190ceb594440d",
+    "url": "/post-redux/static/js/runtime-main.2f8332b2.js"
+  }
+]);
